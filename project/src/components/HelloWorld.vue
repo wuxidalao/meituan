65a4c757d0442a4091f4beac78670783d7e537a4
wuxidalao <yatouxixi520@qq.com>
2019-08-02 17:53:55 +0800

    1

--- project/src/components/HelloWorld.vue
+++ project/src/components/HelloWorld.vue
@@ -1,17 +1,24 @@
 <template>
   <div class="hello">
-    <p>{{msg}}</p>
+    vuex{{$store.count}}
+    <button type="button" @click="increment">加</button>
+    <button type="button" @click="decrement">减</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
-  }
+  },
+  methods: mapActions([
+    'increment',
+    'decrement'
+  ])
 }
 </script>
 <style scoped>
--- project/src/main.js
+++ project/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Vuex from 'vuex'
+import vuex from './store/index.js'
+
 Vue.use(Vuex)
 
 Vue.config.productionTip = false
@@ -12,6 +14,7 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
+  vuex,
   components: { App },
   template: '<App/>'
 })
